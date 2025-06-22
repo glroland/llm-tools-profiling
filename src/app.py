c6f4032aa24f95a27b5dd0999e904ea7b3e5ed2a
Lee Roland <glroland@hotmail.com>
2025-06-21 21:58:32 -0400

    cleanup

--- src/app.py
+++ src/app.py
@@ -12,10 +12,12 @@ TOGETHER_ENDPOINT = "https://api.together.xyz/v1"
 TOGETHER_TOKEN = os.environ["TOGETHER_API_KEY"]
 TOGETHER_MODELS = [
 #    "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-#    "meta-llama/Llama-3.3-70B-Instruct-Turbo"
+#    "meta-llama/Llama-3.3-70B-Instruct-Turbo",
+#    "Qwen/Qwen2.5-VL-72B-Instruct",
+#    "meta-llama/Llama-4-Scout-17B-16E-Instruct",
 ]
 OPENAI_MODELS = [
-    "gpt-3.5-turbo"
+    "gpt-3.5-turbo",
 ]
 
 was_multiply_invoked = False
@@ -59,7 +61,9 @@ def reset():
     global was_get_age_invoked
     was_get_age_invoked = False
 
-def run_test(llm, tools, test_prompt):
+def run_test(llm, test_prompt):
+    tools = [multiply, get_age]
+
     agent = create_openai_tools_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(
         agent=agent,
@@ -70,12 +74,7 @@ def run_test(llm, tools, test_prompt):
 
     response = agent_executor.invoke({"input": test_prompt})
     output = response["output"]
-
-    if was_multiply_invoked:
-        print (f"PASSED!!!  {output}")
-    else:
-        print (f"FAILED!!!  {output}")
-
+    return output
 
 def test_model(endpoint, token, model_name):
     reset()
@@ -85,11 +84,23 @@ def test_model(endpoint, token, model_name):
                     model=model_name,
                     temperature=0.7)
 
-    tools = [multiply]
-    run_test(llm, tools, "Bob's favorite number is 4.  What is 8 times that number?")
+    output = run_test(llm, "What's the capital of France?")
+    if not was_multiply_invoked and not was_get_age_invoked:
+        print (f"PASSED!!!  {output}")
+    else:
+        print (f"FAILED!!!  {output}")
 
-    tools = [multiply, get_age]
-    run_test(llm, tools, "What is Bob's age times two?")
+    output = run_test(llm, "Bob's favorite number is 4.  What is 8 times that number?")
+    if was_multiply_invoked and not was_get_age_invoked:
+        print (f"PASSED!!!  {output}")
+    else:
+        print (f"FAILED!!!  {output}")
+
+    output = run_test(llm, "What is Bob's age times two?")
+    if was_multiply_invoked and was_get_age_invoked:
+        print (f"PASSED!!!  {output}")
+    else:
+        print (f"FAILED!!!  {output}. Mult={was_multiply_invoked} Age={was_get_age_invoked}")
 
 
 for model in OPENAI_MODELS:
